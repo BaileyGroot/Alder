07b1d61ee83de8e79bc9e0865af17b5b8e914dc0
Grootrix <grootrix@gmail.com>
2021-08-09 14:04:15 +0200

    Add project files.

---
+++ Alder/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Alder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+      
+
+            if (age < 3)
+
+            {
+
+                Console.WriteLine(userName + ", du må gå med ble");
+
+            }
+
+            else if (age < 15)
+
+            {
+
+                Console.WriteLine(userName + ", du må ingenting");
+
+            }
+
+            else if (age < 18)
+
+            {
+
+                Console.WriteLine(userName + ", du må drikke");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine(userName + ", du må stemme og køre bil");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
